7885d5ad74cec093ca347d3a4a570e25f97084bd
Your Name <your@email.com>
2026-06-03 14:30:59 +0200

    Remove health check route

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
-app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "EduReports API is running"
-  });
-});
+
 require("dotenv").config();
 
 const express = require("express");
