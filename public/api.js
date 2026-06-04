19e4a1ed7ce7cf806ef1a093c239e43fa6136405
Your Name <your@email.com>
2026-06-04 13:25:11 +0200

    Fix frontend API URLs and Netlify deployment issues

--- public/api.js
+++ public/api.js
@@ -1,16 +1,14 @@
 function api(url, options = {}) {
 
-  const token =
-    localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
   return fetch(url, {
     ...options,
     headers: {
       "Content-Type": "application/json",
-      Authorization:
-        token
-          ? `Bearer ${token}`
-          : "",
+      Authorization: token
+        ? `Bearer ${token}`
+        : "",
       ...(options.headers || {})
     }
   });
